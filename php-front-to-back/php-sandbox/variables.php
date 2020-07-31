6bffa8eb3e62107ea7ebc6dd545353966eb5cdf1
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-07-31 15:20:28 -0700

    went over other primitive data type values in lesson

--- php-front-to-back/php-sandbox/variables.php
+++ php-front-to-back/php-sandbox/variables.php
@@ -14,10 +14,54 @@
 		- The variable starts with a letter or an underscore
 				-- Only letters, numbers and underscores
 				-- case-sensitive
-	*/
+  */
+
+  //Data Types
+
+  /*
+      String
+        -- can be in double or single quotes
+      Integers
+        -- positive or negative
+      floats
+      Booleans
+      Arrays
+      Objects
+      NULL
+      Resource
+  */
 
 
-	$output = 'Hello World!';
-	echo $output;
+  $output = 'Hello World!';
+  $numb1 = 4;
+  $numb2 = 10;
+	$sum = $numb1 + $numb2;
+
+	$string1 = 'Hello';
+	$string2 = 'world!';
+	//$greeting = $string1 . ' ' . $string2. '!'; // this is concatenation; easier way is using double quotes
+	//$greeting2 = "$string1 $string2"; // single quotes will print exactly what you put in, it wont parse the variable
+
+	//echo $greeting2;
+		// in concatenation in php, you use a dot '.'
+
+		//escape characters
+//	$string3 = 'They\'re Here'; // escape only if it matches single quotes
+	$string3 = "They're Here"; // if double quotes, it's the same thing
+
+//	echo $string3;
+
+
+  $float1 = 4.4;
+  $bool1 = true;
+
+	//echo $sum;
+
+	//constants are values that you don't want to change, like a server name
+
+	define('GREETING', 'Hello Everyone', true); // you need to use the define function and
+		// add the third parameter allows it to be case insensitive
+
+	echo greeting;
 
 	?>
