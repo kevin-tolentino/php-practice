9955685b969ef5797ff869fe74bc84531a7fbe53
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-07-31 15:04:57 -0700

    completed variable rules section

--- php-front-to-back/php-sandbox/variables.php
+++ php-front-to-back/php-sandbox/variables.php
@@ -1,6 +1,23 @@
-<h1><$php
+<?php
+	//single line comment
+	# single line comment
 
-  $output = 'Hello World!';
-  echo $output;
+	/*
 
-?></h1>
+	Multi-line
+	comment
+	*/
+
+	//Variable Rules
+	/*
+		- Prefix with the $, or it will be read as a constant
+		- The variable starts with a letter or an underscore
+				-- Only letters, numbers and underscores
+				-- case-sensitive
+	*/
+
+
+	$output = 'Hello World!';
+	echo $output;
+
+	?>
