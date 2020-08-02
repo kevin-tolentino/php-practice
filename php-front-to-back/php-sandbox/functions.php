076b87df07a7f0ac1ca5e755aa7686565950593b
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-08-01 21:57:21 -0700

    went over returning a number from a written function and also passing args as reference vs passing args by value

--- php-front-to-back/php-sandbox/functions.php
+++ php-front-to-back/php-sandbox/functions.php
@@ -21,4 +21,32 @@
   // sayHello('Joe');
 
   // sayHello('Tim');
+
+  function addNumber($num1, $num2){
+    return $num1 + $num2;
+
+  }
+
+  //echo addNumber(1,2); // echo what is returned from the function
+
+
+  // Passing args by ref
+
+  $myNum = 10;
+
+  function addFive($num){ // passed by value
+    $num = $num + 5;
+  }
+
+    function addTen(&$num){ // passed by reference
+    $num = $num + 10;
+  }
+
+  addFive($myNum);
+
+  echo "Value: $myNum<br>";
+
+  addTen($myNum);
+
+  echo "Value: $myNum<br>"
 ?>
