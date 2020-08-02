f02f80235122f104f814ab720c3376b81259d77a
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-08-01 21:22:51 -0700

    went over do-while loops and for-each loops

--- php-front-to-back/php-sandbox/loops.php
+++ php-front-to-back/php-sandbox/loops.php
@@ -1,23 +1,54 @@
 <?php
 
-  #Loops - Execute code set number of times
+#Loops - Execute code set number of times
 
-  //For, While, Do-while, Foreach
+//For, While, Do-while, Foreach
 
-  //For loop
-  //params - init, condition, incrementor
+//For loop
+//params - init, condition, incrementor
 
-  // for ($i=0; $i < 10; $i++){
-  //   echo 'Number '.$i;
-  //   echo '<br>';
-  // }
+// for ($i=0; $i < 10; $i++){
+//   echo 'Number '.$i;
+//   echo '<br>';
+// }
 
-  //while loop - usually when you don't know when the loop will stop
-  //params - condition
+//while loop - usually when you don't know when the loop will stop
+//params - condition
 
-  $i = 0;
-  while($i < 10){
-    echo $i;
-    echo '<br>';
-    $i++;
-  }
+// $i = 0;
+// while($i < 10){
+//   echo $i;
+//   echo '<br>';
+//   $i++;
+// }
+
+//Do..While
+// params - condition
+
+// $i = 0;
+
+// do{
+//   echo 'number '. $i;
+//   echo '<br>';
+//   $i++;
+// }
+
+// while ($i < 10);
+
+//Foreach loop - for arrays
+
+// $people = array ('brad', 'jose', 'william');
+
+// foreach($people as $person){
+//   echo $person;
+//   echo '<br>';
+// }
+
+
+//for each with an associative array
+$people = array('brad' => 'brad@gmail.com', 'jose' => 'jose@gmail.com', 'william' => 'will@gmail.com');
+
+foreach ($people as $person => $email) {
+  echo $person. ': ' . $email;
+  echo '<br>';
+}
