9830b341a19bcc6c1a53e3fc2925e3395a9930eb
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-08-01 20:39:30 -0700

    began array exercise and went over indexed arrays

---
+++ php-front-to-back/php-sandbox/array.php
@@ -0,0 +1,29 @@
+<?php
+  //Array - variable that holds multiple values
+   // There are three types
+    // -Indexed
+    // -Associative
+    // -Multi-Dimenstional
+
+  //Indexed Arrayed
+  $people = array('Kevin', 'Jeremy', 'Sara');
+
+ // echo $people[0];
+
+ $ids = array(23, 44, 12);
+//  echo $ids[1];
+
+$cars = ['Honda', 'Toyota', 'Ford'];
+// echo $cars[2];
+
+$cars[] = 'chevy';
+
+// echo $cars[3];
+
+  // print_r($cars);
+
+  var_dump($cars);
+
+
+
+  ?>
