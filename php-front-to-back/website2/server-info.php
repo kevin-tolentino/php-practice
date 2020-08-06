47a063aa3a9971b7258473570f194755a1e32d6f
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-08-05 22:16:55 -0700

    went through the $server superglobal

--- php-front-to-back/website2/server-info.php
+++ php-front-to-back/website2/server-info.php
@@ -4,10 +4,16 @@
 
   //Create Server Array
   $server = [
-    'Host Server Name' => $_SERVER['SERVER_NAME'],
-    'Host Header' => $_SERVER['HTTP_HOST']
+    'Host Server Name' => $_SERVER['SERVER_NAME'],// shows the server host the script is executing in
+    'Host Header' => $_SERVER['HTTP_HOST'], // shows the headers of current http request
+    'Server Software' => $_SERVER['SERVER_SOFTWARE'], // shows what server you're running
+    'Document Root' => $_SERVER['DOCUMENT_ROOT'], // shows you the document root
+    'Current Page' => $_SERVER['PHP_SELF'], //shows the filename of the executed script
+    'Script Name' => $_SERVER['SCRIPT_NAME'], //the current script's path
+    'Script Name' => $_SERVER['SCRIPT_FILENAME'] //gives you the absolute path
+
   ];
-  echo $server['Host Header'];
+  // print_r($server);
 
   // Create Client Array
 
