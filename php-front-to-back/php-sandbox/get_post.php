e1d9afbba062afd58d65d51a48183d7eb3def589
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-08-06 22:42:50 -0700

    made get_post file and created beginning of standard form

---
+++ php-front-to-back/php-sandbox/get_post.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My website</title>
+</head>
+<body>
+  <form action="">
+    <div>
+    <label for="">Name</label><br>
+    <input type="text" name="name">
+    </div>
+    <div>
+    <label for="">Email</label><br>
+    <input type="submit" value="Submit">
+    </div>
+  </form>
+</body>
+</html>
