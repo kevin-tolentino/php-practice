451eadbdc8a25068909725f0e805787f2045ded0
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-08-12 21:12:36 -0700

    went through stren() and strpos() string methods

--- php-front-to-back/php-sandbox/string_functions.php
+++ php-front-to-back/php-sandbox/string_functions.php
@@ -2,6 +2,19 @@
 // substr()
 // returns a portion of a string
 
-$output = substr('Hello', 2);
+// $output = substr('Hello', -2);
+// $output = substr('Hello', 1, 3);
 
-echo $output;
+// echo $output;
+
+// strlen() - returns length of a string
+//
+
+// $output = strlen("hello world");
+
+// echo $output;
+
+//strpos() - finds the position of the first occurence of a substring
+
+// $output = strpos('Hello World', 'o');
+// echo $output;
