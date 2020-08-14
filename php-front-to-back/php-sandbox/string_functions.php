f83c6a73057e8fedd3b740c89c9207047ef3cf1e
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-08-13 19:23:30 -0700

    went through remainind common string methods

--- php-front-to-back/php-sandbox/string_functions.php
+++ php-front-to-back/php-sandbox/string_functions.php
@@ -18,3 +18,60 @@
 
 // $output = strpos('Hello World', 'o');
 // echo $output;
+
+// $output = strrpos('Hello World', 'o'); // finds last occurrence
+// echo $output;
+
+//trim() - trims whitespace
+
+// $text = 'Hello World                ';
+// var_dump($text);
+
+// $trimmed = trim($text);
+// var_dump($trimmed); // using for form input
+
+//strtoupper = Make everything to uppercase
+
+// $output = strtoupper('hello world');
+// echo $output;
+
+// $output = strtolower('Hello world');
+// echo $output;
+
+//ucwords() - Captialize Every Word;
+
+// $output = ucwords('hello world');
+// echo $output;
+
+//str_replace() - replace all occurences of a search string with a replacement
+
+// $text = 'Hello World';
+// $output = str_replace('World', 'Everyone', $text);
+// echo $output;
+
+//is_string()
+//Check if string
+
+// $val = '22';
+// $output = is_string($val);
+
+// echo $output;
+
+// $values = [true, false, null, 'abc', 33, '33', 22.4, '22.4', '', ' ', 0, '0'];
+
+// foreach($values as $value){
+//   if(is_string($value)){
+//     echo "{$value} is a string<br>";
+//   }
+// }
+
+//gzcompress() - Compress a string
+
+$string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex beatae magni deleniti minima temporibus illo natus facere numquam. Voluptatum, corrupti.';
+
+$compressed = gzcompress($string);
+echo $compressed;
+
+$original = gzuncompress($compressed);
+
+echo $original;
